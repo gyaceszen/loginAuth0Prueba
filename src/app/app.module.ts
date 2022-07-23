a8cbd0822fea1e6cef189d039a77bea3bc7243d7
gyaceszen <gaston.vy@gmail.com>
2022-07-23 18:35:48 -0300

    ruteo creado, con diseño de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
